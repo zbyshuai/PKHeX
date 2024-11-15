0851d1adc166b853841c09cf254f78c0cc412631
Daivd Wong <55487945+zbyshuai@users.noreply.github.com>
2024-11-15 10:12:00 +0800

    Update BlueberryQuestRecord9.cs

--- PKHeX.Core/Saves/Substructures/Gen9/BlueberryQuestRecord9.cs
+++ PKHeX.Core/Saves/Substructures/Gen9/BlueberryQuestRecord9.cs
@@ -4,6 +4,9 @@ namespace PKHeX.Core;
 
 public sealed class BlueberryQuestRecord9(SAV9SV sav, SCBlock block) : SaveBlock<SAV9SV>(sav, block.Data)
 {
+    public uint Quests1  { get => ReadUInt32LittleEndian(Data[0x4..]); set => WriteUInt32LittleEndian(Data[0x4..], value); }
+    public uint Quests2  { get => ReadUInt32LittleEndian(Data[0x50..]); set => WriteUInt32LittleEndian(Data[0x50..], value); }
+    public uint Quests3  { get => ReadUInt32LittleEndian(Data[0x9c..]); set => WriteUInt32LittleEndian(Data[0x9c..], value); }
     public uint QuestsDoneSolo  { get => ReadUInt32LittleEndian(Data[0x188..]); set => WriteUInt32LittleEndian(Data[0x188..], value); }
     public uint QuestsDoneGroup { get => ReadUInt32LittleEndian(Data[0x18C..]); set => WriteUInt32LittleEndian(Data[0x18C..], value); }
 }
