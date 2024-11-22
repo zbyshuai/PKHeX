a0c9a139c899e16fa3c87203a0ccd31e570578dd
Daivd Wong <55487945+zbyshuai@users.noreply.github.com>
2024-11-22 13:44:00 +0800

    Update BlueberryQuestRecord9.cs

--- PKHeX.Core/Saves/Substructures/Gen9/BlueberryQuestRecord9.cs
+++ PKHeX.Core/Saves/Substructures/Gen9/BlueberryQuestRecord9.cs
@@ -5,8 +5,14 @@ namespace PKHeX.Core;
 public sealed class BlueberryQuestRecord9(SAV9SV sav, SCBlock block) : SaveBlock<SAV9SV>(sav, block.Data)
 {
     public uint Quests1  { get => ReadUInt32LittleEndian(Data[0x4..]); set => WriteUInt32LittleEndian(Data[0x4..], value); }
+    public uint Quests1Process  { get => ReadUInt32LittleEndian(Data[0x8..]); set => WriteUInt32LittleEndian(Data[0x8..], value); }
+
     public uint Quests2  { get => ReadUInt32LittleEndian(Data[0x50..]); set => WriteUInt32LittleEndian(Data[0x50..], value); }
+    public uint Quests2Process  { get => ReadUInt32LittleEndian(Data[0x54..]); set => WriteUInt32LittleEndian(Data[0x54..], value); }
+    
     public uint Quests3  { get => ReadUInt32LittleEndian(Data[0x9c..]); set => WriteUInt32LittleEndian(Data[0x9c..], value); }
+    public uint Quests3Process  { get => ReadUInt32LittleEndian(Data[0xA0..]); set => WriteUInt32LittleEndian(Data[0xA0..], value); }
+    
     public uint QuestsDoneSolo  { get => ReadUInt32LittleEndian(Data[0x188..]); set => WriteUInt32LittleEndian(Data[0x188..], value); }
     public uint QuestsDoneGroup { get => ReadUInt32LittleEndian(Data[0x18C..]); set => WriteUInt32LittleEndian(Data[0x18C..], value); }
 }
